47f60e88eed01b09f2254ae3604ee1504f2ec124
masumbillah6778bd <masumbillah6778bd@gmail.com>
2025-10-03 03:18:03 +0600

    Update and rename script.js to music-lyrics.js

--- js/music-lyrics.js
+++ js/music-lyrics.js
@@ -4,7 +4,7 @@ var _data = '';
 var currentLine = '';
 mounted();
 async function setLyric() {
-  await fetch('lyric.json', {
+  await fetch('js/lyrics/lyric.json', {
     method: 'GET',
   })
     .then((response) => response.json())
